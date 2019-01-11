97f86a773ed93815370ca9e288452ddf09c6d072
Frank Zhang <fzhang@travelzoo.com>
2019-01-11 15:44:15 +1100

    Buttons

--- Extract_From_Delivery_Report.R
+++ Extract_From_Delivery_Report.R
@@ -4,7 +4,7 @@ library(writexl)
 library(RSelenium)
 
 
-# Rselenium ---------------------------------------------------------------
+# Settings-Rselenium ---------------------------------------------------------------
 
 
 eCaps <- list(
@@ -25,5 +25,26 @@ remDr <- remoteDriver(remoteServerAddr = "127.0.0.1"
 remDr$open() 
 remDr$setTimeout(type = "page load", milliseconds = 200000)
 
+# Get buttons-Rselenium ---------------------------------------------------
+
+
 remDr$navigate("https://intranet.travelzoo.com/common/production/DeliveryPeriodReport.aspx")
 
+locale <- remDr$findElement(using = "xpath",'//*[@id="ddlLocales"]')
+locale$sendKeysToElement(list("US"))
+
+start_date <- remDr$findElement(using = "xpath",'//*[@id="txtStartDate"]')
+start_date$sendKeysToElement(list("01/08/2018"))
+
+end_date <- remDr$findElement(using = "xpath",'//*[@id="txtEndDate"]')
+end_date$sendKeysToElement(list("01/01/2019"))
+
+exclude_delivery_checkbox <- remDr$findElement(using = "xpath",'//*[@id="chkExcludeNoDelivery"]')
+#Uncomment if want to exclude results with no delivery
+exclude_delivery_checkbox$clickElement()
+
+include_CPC <- remDr$findElement(using = "xpath",'//*[@id="chkIncludeOnlyCPC"]')
+include_CPC$clickElement()
+
+product_group <- remDr$findElement(using = "xpath",'//*[@id="ddlProductGroup"]')
+product_group$sendKeysToElement(list("Local Deals"))
