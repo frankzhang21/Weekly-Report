9cd965685b0b4b6347364ebdb51be2cf145fe484
Frank Zhang <fzhang@travelzoo.com>
2019-01-18 16:17:28 +1100

    Update

--- Extract_From_Delivery_Report.R
+++ Extract_From_Delivery_Report.R
@@ -2,8 +2,30 @@ library(tidyverse)
 library(rvest)
 library(writexl)
 library(RSelenium)
+library(readxl)
+library(data.table)
+
+# Pre-Settings ------------------------------------------------------------
+
+Country_list <- c("JP", "AU", "SG", "CN", "HK")
+
+week_index <- read_excel("H:/Project/Weekly-Report/Week_Settings_DB.xlsx")
+Production_query_table <- read_excel("H:/Project/Weekly-Report/Week_Settings_DB.xlsx", sheet = 2)
+
+mother_production <- Production_query_table$Production[1]
+sub_production <- Production_query_table$Sub_production
+
+country_table <- data.table(Country=rep(Country_list,nrow(week_index)), Destination = 0,Week=1:nrow(week_index))
 
 
+replace_comma <- partial(str_remove_all, pattern = ",")
+to_number <- compose(as.numeric, replace_comma)
+
+week_index$Start_Date <- str_c(day(week_index$Start_Date), month(week_index$Start_Date), year(week_index$Start_Date), sep = "/")
+week_index$End_Date <- str_c(day(week_index$End_Date), month(week_index$End_Date), year(week_index$End_Date), sep = "/")
+
+Start_date_list <- week_index$Start_Date
+End_date_list <- week_index$End_Date
 # Settings-Rselenium ---------------------------------------------------------------
 
 
@@ -17,12 +39,14 @@ eCaps <- list(
   )
 )
 
-remDr <- remoteDriver(remoteServerAddr = "127.0.0.1" 
-                      , port = 4444
-                      , browserName = "chrome"
-                      ,extraCapabilities = eCaps)#连接Server
+remDr <- remoteDriver(
+  remoteServerAddr =
+    "127.0.0.1", port =
+    4444, browserName =
+    "chrome", extraCapabilities = eCaps
+) # <U+8FDE><U+63A5>Server
 
-remDr$open() 
+remDr$open()
 remDr$setTimeout(type = "page load", milliseconds = 200000)
 
 # Get buttons-Rselenium ---------------------------------------------------
@@ -30,21 +54,64 @@ remDr$setTimeout(type = "page load", milliseconds = 200000)
 
 remDr$navigate("https://intranet.travelzoo.com/common/production/DeliveryPeriodReport.aspx")
 
-locale <- remDr$findElement(using = "xpath",'//*[@id="ddlLocales"]')
-locale$sendKeysToElement(list("US"))
+## Click exclude results with no delivery mannually
 
-start_date <- remDr$findElement(using = "xpath",'//*[@id="txtStartDate"]')
-start_date$sendKeysToElement(list("01/08/2018"))
+for (i in Country_list[1]) {
+  locale <- remDr$findElement(using = "xpath", '//*[@id="ddlLocales"]')
+  locale$sendKeysToElement(list(i))
 
-end_date <- remDr$findElement(using = "xpath",'//*[@id="txtEndDate"]')
-end_date$sendKeysToElement(list("01/01/2019"))
+  for (n in 3:nrow(week_index)) {
+    start_date <- remDr$findElement(using = "xpath", '//*[@id="txtStartDate"]')
+    start_date$clearElement()
+    start_date$sendKeysToElement(list(week_index$Start_Date[n]))
 
-exclude_delivery_checkbox <- remDr$findElement(using = "xpath",'//*[@id="chkExcludeNoDelivery"]')
-#Uncomment if want to exclude results with no delivery
-exclude_delivery_checkbox$clickElement()
+    end_date <- remDr$findElement(using = "xpath", '//*[@id="txtEndDate"]')
+    end_date$clearElement()
+    end_date$sendKeysToElement(list(week_index$End_Date[n]))
 
-include_CPC <- remDr$findElement(using = "xpath",'//*[@id="chkIncludeOnlyCPC"]')
-include_CPC$clickElement()
+    # exclude_delivery_checkbox <- remDr$findElement(using = "xpath", '//*[@id="chkExcludeNoDelivery"]')
+    # # Uncomment if want to exclude results with no delivery
+    # exclude_delivery_checkbox$clickElement()
 
-product_group <- remDr$findElement(using = "xpath",'//*[@id="ddlProductGroup"]')
-product_group$sendKeysToElement(list("Local Deals"))
+    # include_CPC <- remDr$findElement(using = "xpath",'//*[@id="chkIncludeOnlyCPC"]')
+    # include_CPC$clickElement()
+
+    product_group <- remDr$findElement(using = "xpath", '//*[@id="ddlProductGroup"]')
+    product_group$sendKeysToElement(list(mother_production))
+
+    
+
+    for (g in sub_production) {
+      sub_group <- remDr$findElement(using = "xpath", '//*[@id="ddlProductSubGroup"]')
+      
+      sub_group$sendKeysToElement(list(g))
+      
+      sbmt <- remDr$findElement(using = "xpath", '//*[@id="btnSubmit"]')
+      sbmt$clickElement()
+      # Sys.sleep(2)
+
+      pivot_table <- remDr$findElement(using = "xpath",'//*[@id="grdPivot_MT"]')
+
+      pivot_table_text <- pivot_table$getElementText()[[1]]
+      
+      a <- pivot_table_text
+      b <- str_match_all(a,"[:lower:]{1} \\d+ [¥$]{1} (.+\\.\\d{2}) \\d")
+      c <- data.table(b[[1]])
+      d <- c[.N,2]
+      dev_amount <- d$V2
+
+      if(dev_amount=="0.00"){
+        final_dev_amount <- dev_amount
+      } else {
+        final_dev_amount <- str_sub(dev_amount,1,str_locate(dev_amount," ")[1]-1)
+      }
+      
+      if (str_sub(g, 1,1) == "D") {
+        country_table[Country == i & Week==n, c("Destination") := .(Destination + to_number(final_dev_amount))]
+      } else {
+        country_table[Country == i & Week==n, c(g) := .(to_number(final_dev_amount))]
+      }
+    }
+  }
+}
+
--- Marketing&production Summary.R
+++ Marketing&production Summary.R
@@ -9,8 +9,8 @@ library(rvest)
 
 # Functions dim -----------------------------------------------------------
 
-replace_comma <- partial(str_remove_all,pattern=",")
-to_interger <- compose(as.integer,replace_comma)
+replace_comma <- partial(str_remove_all, pattern = ",")
+to_interger <- compose(as.integer, replace_comma)
 
 # Settings-Rselenium ---------------------------------------------------------------
 
@@ -25,12 +25,14 @@ eCaps <- list(
   )
 )
 
-remDr <- remoteDriver(remoteServerAddr = "127.0.0.1" 
-                      , port = 4444
-                      , browserName = "chrome"
-                      ,extraCapabilities = eCaps)#连接Server
+remDr <- remoteDriver(
+  remoteServerAddr =
+    "127.0.0.1", port =
+    4444, browserName =
+    "chrome", extraCapabilities = eCaps
+) # <U+8FDE><U+63A5>Server
 
-remDr$open() 
+remDr$open()
 remDr$setTimeout(type = "page load", milliseconds = 200000)
 
 
@@ -38,61 +40,61 @@ remDr$setTimeout(type = "page load", milliseconds = 200000)
 
 remDr$navigate("https://intranet.travelzoo.com/dashboard/subscriber/")
 
-#Ending date for net members
-net_member_end_date <- remDr$findElement(using = "xpath",'//*[@id="txtSSDateEnding"]')#Assign to var
-net_member_end_date$clearElement()#Clear date input form
-report_week_monday <- Sys.Date()-3
-text_format_mondy <- str_c(day(report_week_monday),month(report_week_monday),year(report_week_monday),sep="/")
-net_member_end_date$sendKeysToElement(list(text_format_mondy))#enter right date
+# Ending date for net members
+net_member_end_date <- remDr$findElement(using = "xpath", '//*[@id="txtSSDateEnding"]') # Assign to var
+net_member_end_date$clearElement() # Clear date input form
+report_week_monday <- Sys.Date() - 3
+text_format_mondy <- str_c(day(report_week_monday), month(report_week_monday), year(report_week_monday), sep = "/")
+net_member_end_date$sendKeysToElement(list(text_format_mondy)) # enter right date
 
-Go_button <- remDr$findElement(using = "xpath",'//*[@id="btnSSSubmit"]')
+Go_button <- remDr$findElement(using = "xpath", '//*[@id="btnSSSubmit"]')
 Go_button$clickElement()
 # Extract net members for each country
 Response <- read_html(remDr$getPageSource()[[1]])
-net_member_table <- html_table(Response,fill = TRUE)[[2]]
-names(net_member_table) <- c("Country",str_c(c("Top20","Newsflash","Local_Deails"),"Subscriptions",sep="_"),"Subscribers")
-new_member_by_country <- tibble(Country=net_member_table$Country[2:7],Net_Member=to_interger(net_member_table$Subscribers[2:7]))
+net_member_table <- html_table(Response, fill = TRUE)[[2]]
+names(net_member_table) <- c("Country", str_c(c("Top20", "Newsflash", "Local_Deails"), "Subscriptions", sep = "_"), "Subscribers")
+new_member_by_country <- tibble(Country = net_member_table$Country[2:7], Net_Member = to_interger(net_member_table$Subscribers[2:7]))
 final_table <- data.table(new_member_by_country)
 
 # Data for New&Unsubs -----------------------------------------------------
 
-#junmp to new&unsubs
+# junmp to new&unsubs
 
 new_unsub_start_date <- "1/1/2019"
-new_unsub_button <- remDr$findElement(using = "xpath",'//*[@id="rptTabs_ctl02_lnkTab"]')
+new_unsub_button <- remDr$findElement(using = "xpath", '//*[@id="rptTabs_ctl02_lnkTab"]')
 new_unsub_button$clickElement()
 
-#Set dates first
-new_unsub_start_date_form <- remDr$findElement(using = "xpath",'//*[@id="txtSUTFrom"]')
+# Set dates first
+new_unsub_start_date_form <- remDr$findElement(using = "xpath", '//*[@id="txtSUTFrom"]')
 new_unsub_start_date_form$clearElement()
 new_unsub_start_date_form$sendKeysToElement(list(new_unsub_start_date))
 
-new_unsub_end_date_form <- remDr$findElement(using = "xpath",'//*[@id="txtSUTTo"]')
+new_unsub_end_date_form <- remDr$findElement(using = "xpath", '//*[@id="txtSUTTo"]')
 new_unsub_end_date_form$clearElement()
 new_unsub_end_date_form$sendKeysToElement(list(text_format_mondy))
 
-#Loop over different country
+# Loop over different country
 
 for (i in final_table$Country) {
-  
-
-locale_button <- remDr$findElement(using = "xpath",'//*[@id="drpSUTLocale"]')
-Country_name <- i
-locale_button$sendKeysToElement(list(Country_name))
+  locale_button <- remDr$findElement(using = "xpath", '//*[@id="drpSUTLocale"]')
+  Country_name <- i
+  locale_button$sendKeysToElement(list(Country_name))
 
 
 
-scale <- remDr$findElement(using = "xpath",'//*[@id="drpSUTScale"]')
-scale$sendKeysToElement(list("monthly"))
+  scale <- remDr$findElement(using = "xpath", '//*[@id="drpSUTScale"]')
+  scale$sendKeysToElement(list("monthly"))
 
-Go_button <- remDr$findElement(using = "xpath",'//*[@id="btnSUTGo"]')
-Go_button$clickElement()
+  Go_button <- remDr$findElement(using = "xpath", '//*[@id="btnSUTGo"]')
+  Go_button$clickElement()
 
-Response <- read_html(remDr$getPageSource()[[1]])
-new_unsub_table <- html_table(Response,fill = TRUE)[[2]]
+  Response <- read_html(remDr$getPageSource()[[1]])
+  new_unsub_table <- html_table(Response, fill = TRUE)[[2]]
 
-final_table[Country==Country_name,c("New_sub","Un_sub"):=.(to_interger(new_unsub_table$Subscribes[1]),
-            to_interger(new_unsub_table$`Total Unsubscribes`[1]))]
+  final_table[Country == Country_name, c("New_sub", "Un_sub") := .(
+    to_interger(new_unsub_table$Subscribes[1]),
+    to_interger(new_unsub_table$`Total Unsubscribes`[1])
+  )]
 }
 
 
@@ -100,62 +102,61 @@ final_table[Country==Country_name,c("New_sub","Un_sub"):=.(to_interger(new_unsub
 
 remDr$navigate("https://intranet.travelzoo.com/office/top20/eu/?tzlocale=10")
 
-##Manually adjust week number and show stats
+## Manually adjust week number and show stats
 
 
 for (i in final_table$Country) {
-  
-  
-  locale_button <- remDr$findElement(using = "xpath",'//*[@id="intranetHeaderBreadcrumbs"]/form/select')
+  locale_button <- remDr$findElement(using = "xpath", '//*[@id="intranetHeaderBreadcrumbs"]/form/select')
   Country_name <- i
   locale_button$sendKeysToElement(list(Country_name))
-  
+
   Sys.sleep(1.5)
-  
+
   Response <- read_html(remDr$getPageSource()[[1]])
-  production_table <- html_table(Response,fill = TRUE)[[7]]
-  clicks_table <- html_table(Response,fill = TRUE)[[6]]
+  production_table <- html_table(Response, fill = TRUE)[[7]]
+  clicks_table <- html_table(Response, fill = TRUE)[[6]]
   clicks_string <- names(clicks_table)[5]
-  names(production_table) <- c("names","data")
-  final_table[Country==Country_name,c("Delivery","Clicks","Open_Count","Primary","Secondary"):=.(to_interger(production_table$data[1]),#Newsletters Sent
-                                                             to_interger(production_table$data[2]),#Newsletters Clicks
-                                                             to_interger(production_table$data[4]),#Open Count
-                                                             to_interger(str_match(clicks_string,"Primary (.+)\\n")[,2]),#Primary Clicks
-                                                             to_interger(str_match(clicks_string,"Secondary (.+)")[,2]))]#Secondary Clicks
+  names(production_table) <- c("names", "data")
+  final_table[Country == Country_name, c("Delivery", "Clicks", "Open_Count", "Primary", "Secondary") := .(
+    to_interger(production_table$data[1]), # Newsletters Sent
+    to_interger(production_table$data[2]), # Newsletters Clicks
+    to_interger(production_table$data[4]), # Open Count
+    to_interger(str_match(clicks_string, "Primary (.+)\\n")[, 2]), # Primary Clicks
+    to_interger(str_match(clicks_string, "Secondary (.+)")[, 2])
+  )] # Secondary Clicks
 }
 
 
 
-reshape_version <- melt(final_table,id.vars = "Country") %>% 
-  spread(Country,value) %>% 
-  setcolorder(c("variable","JP","AU","CN","HK","TW","AE"))
+reshape_version <- melt(final_table, id.vars = "Country") %>%
+  spread(Country, value) %>%
+  setcolorder(c("variable", "JP", "AU", "CN", "HK", "TW", "AE"))
 
-write_xlsx(as.data.frame(reshape_version),"H:/Report/Weekly/Report parts/Market_Production.xlsx")
+write_xlsx(as.data.frame(reshape_version), "H:/Report/Weekly/Report parts/Market_Production.xlsx")
 
 
 # Top20 CTR Report --------------------------------------------------------
 
-Country_list <- c("AE","AU","CA","CN","DE","ES","FR","HK","JP","TW","UK","US")
+Country_list <- c("AE", "AU", "CA", "CN", "DE", "ES", "FR", "HK", "JP", "TW", "UK", "US")
 
-Top20_CTR <- data.table(Country=Country_list,CTR="cha",Open_rate=100)
+Top20_CTR <- data.table(Country = Country_list, CTR = "cha", Open_rate = 100)
 
 for (i in Country_list) {
-  
-  
-  locale_button <- remDr$findElement(using = "xpath",'//*[@id="intranetHeaderBreadcrumbs"]/form/select')
+  locale_button <- remDr$findElement(using = "xpath", '//*[@id="intranetHeaderBreadcrumbs"]/form/select')
   Country_name <- i
   locale_button$sendKeysToElement(list(Country_name))
-  
+
   Sys.sleep(1.5)
-  
+
   Response <- read_html(remDr$getPageSource()[[1]])
-  production_table <- html_table(Response,fill = TRUE)[[7]]
-  names(production_table) <- c("names","data")
-  Top20_CTR[Country==Country_name,c("CTR","Open_rate"):=.(str_remove_all(production_table$data[3],"%"),
-                                                                           to_interger(str_remove_all(production_table$data[5],"%")))]
+  production_table <- html_table(Response, fill = TRUE)[[7]]
+  names(production_table) <- c("names", "data")
+  Top20_CTR[Country == Country_name, c("CTR", "Open_rate") := .(
+    str_remove_all(production_table$data[3], "%"),
+    to_interger(str_remove_all(production_table$data[5], "%"))
+  )]
 }
 
-Top20_CTR[,CTR:=as.numeric(CTR)]
+Top20_CTR[, CTR := as.numeric(CTR)]
 
-write_xlsx(as.data.frame(Top20_CTR),"H:/Report/Weekly/Report parts/Top20_CTR.xlsx")
-
+write_xlsx(as.data.frame(Top20_CTR), "H:/Report/Weekly/Report parts/Top20_CTR.xlsx")
